426318df382654e430c6d2b26fcff97386cd5179
sunzero2 <60477554+sunzero2@users.noreply.github.com>
2021-01-04 13:52:49 +0900

    2021-01-04 Jsoup Study

--- jsoup/src/main/java/JsoupTest.java
+++ jsoup/src/main/java/JsoupTest.java
@@ -5,17 +5,48 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsoupTest {
     public static void main(String[] args) throws IOException {
         // 1. return Connection.Response
-        Connection.Response testUrl = Jsoup.connect("http://www.naver.com").execute();
-        int code = testUrl.statusCode(); // HTTP Status Code
-        Document html = testUrl.parse(); // HTML
-        Elements eles = html.select("img");
-        for (Element ele: eles) {
-            System.out.println(ele.attr("data-src"));
-            // 2. replay 1
+        Connection.Response testUrl = getUrlConnectionResponse("https://www.naver.com");
+        List<String> elements = htmlParser(testUrl);
+//        System.out.println(elements);
+    }
+
+    public static Connection.Response getUrlConnectionResponse(String url) throws IOException {
+        return Jsoup.connect(url).execute();
+    }
+
+    public static List<String> htmlParser(Connection.Response response) throws IOException {
+        List<String> elements = new ArrayList<>();
+        System.out.println(response.statusMessage());
+        int code = response.statusCode();
+        if (code == 200) {
+            Document html = response.parse();
+
+            // get img
+            Elements imgs = html.select("img");
+            for (Element img : imgs) {
+                if (img.attr("data-src").length() > 0) {
+                    elements.add(img.attr("data-src")); // src, data-src
+                } else {
+                    elements.add(img.attr("src"));
+//                    System.out.println(img);
+                }
+            }
+
+            // get a
+            Elements links = html.select("a");
+            for (Element link : links) {
+                if (link.attr("href").indexOf("h") == 0) {
+                    elements.add(link.attr("href"));
+                }
+            }
         }
+
+        return elements;
     }
 }
