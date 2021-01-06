b5965c7ebcd63ce51682b407512d23a6e9ea3684
이혜영 <lhy@ngle.co.kr>
2021-01-07 01:42:47 +0900

    2021-01-07 React state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,12 +5,43 @@ import Content from './components/Content';
 import Subject from './components/Subject';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mode: 'read',
+      subject: { title: 'WEB', sub: 'world wide web!' },
+      welcome: { title: 'Welcome', desc: 'Hello, React!!!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is information' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' },
+      ]
+    }
+  }
+
   render() {
+    let _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!" />
-        <TOC />
-        <Content title="HTML" des="HTML is HyperText Markup Language." />
+        <header>
+          <h1><a href="/" onClick={(event) => {
+            this.setState({mode: 'welcome'});
+            event.preventDefault();
+          }}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub} /> */}
+        <TOC data={this.state.contents} />
+        <Content title={_title} des={_desc} />
       </div>
     )
   }
--- react-app/src/components/TOC.js
+++ react-app/src/components/TOC.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
   render() {
+    let lists = [];
+    const data = this.props.data;
+
+    for (let i = 0; i < data.length; i++) {
+      lists.push(<li key={data[i].id}><a href={`/content/${data[i].id}`}>{data[i].title}</a></li>);
+    }
+
     return (
       <nav>
         <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.css">CSS</a></li>
-          <li><a href="3.javascript">Javascript</a></li>
+          {lists}
         </ul>
       </nav>
     )
